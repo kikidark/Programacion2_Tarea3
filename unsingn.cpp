c9abbed646213fb11db6ea1494b7ccae9546fce5
kikidark <kikidark@users.noreply.github.com>
2020-03-12 08:00:23 -0600

    Agrege tarea 3 Progra

---
+++ unsingn.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#
+
+int main() {
+
+	std::cout << "char ocupa: " << sizeof(char) << " bytes"<<"\n";
+	std::cout << "unsigned char ocupa:" << sizeof (unsigned char) << " bytes" <<"\n";
+	std::cout << "int ocupa: " << sizeof(int) << " bytes" << "\n";
+	std::cout << "unsigned int ocupa: " << sizeof(unsigned int) << " bytes" << "\n";
+	std::cout << "unsigned short int ocupa: " << sizeof(unsigned short int) << " bytes" << "\n";
+	std::cout << "short int ocuopa: " << sizeof(short int) << " bytes" << "\n";
+	std::cout << "long int ocupa: " << sizeof(long int) << " bytes" << "\n";
+	std::cout << "signed long int ocupa: " << sizeof(signed long int) << " bytes" << "\n";
+	std::cout << "unsigned long int ocupa: " << sizeof(unsigned long int) << " bytes" << "\n";
+	std::cout << "long long int ocupa: " << sizeof(long long int) << " bytes" << "\n";
+	std::cout << "unsigned long long int ocupa: " << sizeof(unsigned long long int) << " bytes" << "\n";
+	std::cout << "float ocupa: " << sizeof(float) << " bytes" << "\n";
+	std::cout << "double ocupa: " << sizeof(double) << " bytes" << "\n";
+	std::cout << "long double ocupa: " << sizeof(long double) << " bytes" << "\n";
+	
+	
+	
+
+}
